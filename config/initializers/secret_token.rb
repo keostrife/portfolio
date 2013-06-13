90dcc2e836d11e605ec6b2e2d605badcbf85cb89
Keo Strife <keo_strife@yahoo.com>
2013-06-13 15:23:31 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portfolio::Application.config.secret_token = '34a0c3d18c7d88dcbb474417e71e478e9c67746df430a3d9b373b0ca8888fffe37ee5eaf985e22feb8758d63bdcbfabb6d3e81fbe23fafad305f5979e1721856'
