ca4db109740a7c07dd943ecf81342b821e5187c9
Keo Strife <keo_strife@yahoo.com>
2013-06-30 10:54:01 -0400

    show name

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,7 +1,14 @@
 class SessionController < ApplicationController
 	def facebook
-  		respond_to do |format|
-  		      format.json { render:json => params }
-  		  end
+		if(params.has_key?(:name))
+			session[:name] = params[:name]
+			session[:id] = params[:id]
+			session[:email] = params[:email]
+			session[:facebook] = params[:facebook]
+			session[:init] = 'yes'
+	  		respond_to do |format|
+	  		    format.json { render:json => params }
+	  		end
+  		end
   	end
 end
