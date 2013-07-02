80529af83dc0f0dd8ef06c77af07418ffad0a65f
Keo Strife <keo_strife@yahoo.com>
2013-07-02 17:03:49 -0400

    update layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 	def userInfo(attr)
 		if attr == 'user'
 			if session[:init] == 'yes'
-				'<img alt="" class="gravatar" src="https://secure.gravatar.com/avatar/'+Digest::MD5.hexdigest(session[:email].downcase)+'">' + session[:name]
+				"<img alt=\"User\" class=\"gravatar\" src=\"https://secure.gravatar.com/avatar/#{Digest::MD5.hexdigest(session[:email].downcase)}\">" + session[:name]
 			else
 				'<span class="symbol">U</span> Guest'
 			end
