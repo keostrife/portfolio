1f5f3ec37c0da611fba8570a185f0217c5a32cb0
Keo Strife <keo_strife@yahoo.com>
2013-07-03 01:34:58 -0400

    permission facebook
    sdfsdf

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
 			if session[:init] == 'yes'
 				''
 			else
-				'<span class="symbol"></span><ul><li><fb:login-button width="100" max-rows="1"></fb:login-button></li></ul>'
+				'<span class="symbol"></span><ul><li><fb:login-button perms="email" width="100" max-rows="1"></fb:login-button></li></ul>'
 			end
 		end
 	end
