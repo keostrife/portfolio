39c9294ebcde2cbcfa3fd5e7c8dd0d5accf95288
Keo Strife <keo_strife@yahoo.com>
2013-07-03 11:44:58 -0400

    general

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,12 @@
 module ApplicationHelper
+	def gravatar_for(email)
+		"<img alt=\"user\" class=\"gravatar\" style=\"width: 20px;\" src=\"https://secure.gravatar.com/avatar/#{Digest::MD5::hexdigest(email.downcase)}\">"
+	end
+
 	def userInfo(attr)
 		if attr == 'user'
 			if session[:init] == 'yes'
-				"<img alt=\"User\" class=\"gravatar\" style=\"width: 20px;\" src=\"https://secure.gravatar.com/avatar/#{Digest::MD5::hexdigest(session[:email].to_s.downcase)}\"> #{session[:name]}"
+				"#{session[:email].to_s} #{session[:name]}"
 				#session[:name]
 			else
 				'<span class="symbol">U</span> Guest'
