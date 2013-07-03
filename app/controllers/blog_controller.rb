0e0888715b036a966992137e669fdc2b2494ca9e
Keo Strife <keo_strife@yahoo.com>
2013-07-03 16:38:24 -0400

    sdfdsf

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -11,6 +11,7 @@ class BlogController < ApplicationController
   def commentnew
     if params.has_key?(:postID)
       @comment = Comment.new(userID: params[:userID], postID: params[:postID], user_email: params[:user_email].to_s, user_name: params[:user_name].to_s, user_facebook: params[:user_facebook].to_s, comment_content: params[:comment].to_s)
+      @comment.save
       respond_to do |format|
           format.json { render:json => params }
       end
