0fb1aaee99b5bfc95df9e4157f9a116fc4e16d51
Keo Strife <keo_strife@yahoo.com>
2013-07-03 15:37:44 -0400

    create comment

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -10,7 +10,7 @@ class BlogController < ApplicationController
   end
 
   def create
-  	@comment = Comment.new(userID: params[:comment][:userID], postID: params[:comment][:postID], user_email: params[:comment][:user_email], user_name: params[:comment][:user_name], user_facebook: params[:comment][:user_facebook], comment_content: params[:comment][:content])
+  	@comment = Comment.new(userID: params[:comment][:userID], postID: params[:comment][:postID], user_email: params[:comment][:user_email].to_s, user_name: params[:comment][:user_name].to_s, user_facebook: params[:comment][:user_facebook].to_s, comment_content: params[:comment][:content].to_s)
     if @comment.save
       redirect_to "/blog/#{params[:comment][:postID]}"
     end
