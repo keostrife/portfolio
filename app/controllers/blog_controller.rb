8dbd914cfad85f972afe1a864ab19129d38e41e4
Keo Strife <keo_strife@yahoo.com>
2013-07-03 16:04:33 -0400

    sdfdsf

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -6,11 +6,10 @@ class BlogController < ApplicationController
   def show
   	@post = Blog.find(params[:id])
   	@comments = Comment.where(postID: params[:id])
-    @comment = Comment.new
   end
 
   def create
-  	@comment = Comment.new(userID: params[:comment][:userID], postID: params[:comment][:postID], user_email: params[:comment][:user_email].to_s, user_name: params[:comment][:user_name].to_s, user_facebook: params[:comment][:user_facebook].to_s, comment_content: params[:comment][:content].to_s)
+  	@comment = Comment.new(userID: session[:id], postID: params[:comment][:postID], user_email: session[:email].to_s, user_name: session[:name].to_s, user_facebook: session[:facebook].to_s, comment_content: params[:comment][:content].to_s)
     if @comment.save
       redirect_to "/blog/#{params[:comment][:postID]}"
     end
